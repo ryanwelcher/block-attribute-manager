3f4a2cc17e61fda12bd6f8740ef876fa25de5d13
Ryan Welcher <me@ryanwelcher.com>
2022-07-05 14:13:45 -0400

    Eject the code.

--- src/withGlobals.js
+++ src/withGlobals.js
@@ -1,28 +1,23 @@
-import type { DecoratorFunction } from "@storybook/addons";
 import { useEffect, useGlobals } from "@storybook/addons";
+export const withGlobals = (StoryFn, context) => {
+  const [{ myAddon }] = useGlobals(); // Is the addon being used in the docs panel
 
-export const withGlobals: DecoratorFunction = (StoryFn, context) => {
-  const [{ myAddon }] = useGlobals();
-  // Is the addon being used in the docs panel
   const isInDocs = context.viewMode === "docs";
-
   useEffect(() => {
     // Execute your side effect here
     // For example, to manipulate the contents of the preview
     const selectorId = isInDocs
       ? `#anchor--${context.id} .docs-story`
       : `#root`;
-
     displayToolState(selectorId, {
       myAddon,
       isInDocs,
     });
   }, [myAddon]);
-
   return StoryFn();
 };
 
-function displayToolState(selector: string, state: any) {
+function displayToolState(selector, state) {
   const rootElement = document.querySelector(selector);
   let preElement = rootElement.querySelector("pre");
 
